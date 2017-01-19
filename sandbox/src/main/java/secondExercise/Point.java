c4cdf1410139df0f107a4926492c924178047125
Dmitry Teterkin <teterkin@tut.by>
2017-01-19 20:55:33 +0300

    доработал для функции. но одновременно не получилось запустить функцию и метод.

--- sandbox/src/main/java/secondExercise/Point.java
+++ sandbox/src/main/java/secondExercise/Point.java
@@ -10,17 +10,16 @@ public class Point {
   public double x; // координата точки по оси Х;
   public double y; // координата точки по оси Y;
 
-
-  // Функция, вычисляющая расстояние между двумя точками
   public static double distance(Point p1, Point p2) {
-    return Math.sqrt( Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y-p2.y, 2) );
+
+    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
   }
 
-
-  // Конструктор для создания точек при вызове из segmentLength
+/** раскоментарить для проверки пункта 4
+ //  Конструктор для создания точек
   Point(double x, double y) {
     this.x = x;
     this.y = y;
-  }
-
+ }
+*/
 }
--- sandbox/src/main/java/secondExercise/segmentLength.java
+++ sandbox/src/main/java/secondExercise/segmentLength.java
@@ -6,13 +6,27 @@ package secondExercise;
  */
 public class segmentLength {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
+// пункт 2 - 3  задания
+    Point p1 = new Point(); // создаем точку 1
+    Point p2 = new Point(); // создаем точку 2
+    p1.x = 2.0;
+    p1.y = 12.0;
+    p2.x = -15.0;
+    p2.y = -90.0;
 
-// задаем координаты точек
+    System.out.println("Расстояние между точками А(" + p1.x + "," + p1.y + ") и B(" + p2.x + "," + p2.y + ") = "+ Point.distance(p1, p2));
+  // окончание пунктов 2 - 3
+
+  }
+ // */
+/** раскоментарить для проверки пункта 4
+// задаем координаты точек для метода
     Point p1 = new Point(0.0, 0.0);
     Point p2 = new Point(9.0, 0.0);
 
 // вывод результата вычисления длины отрезка
     System.out.println("Расстояние между точками А("+ p1.x + "," + p1.y + ") и B(" + p2.x + "," + p2.y +") = " + Point.distance(p1, p2));
   }
+*/
 }
